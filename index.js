c4d50c728bcece2496a0395c865b9435fba50a5a
braum852 <julian.teo59@yahoo.com>
2021-11-17 14:37:41 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching (property, value) {
+    return property.filter(possibleMatch =>
+        possibleMatch.toLowerCase() === value.toLowerCase()
+      )
+}
+
+function fuzzyMatch(property, value) {
+    return property.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(value.toLowerCase()) === 0
+    )
+  }
+
+  function matchName(property, value) {
+    return property.filter( record => record.name === value)
+  }
